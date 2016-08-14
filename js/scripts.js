1c37e7983bae828ac68cc22f5b0e55fb39bbb590
meadumandal <meadumandal@yahoo.com>
2016-08-14 12:06:37 +0800

    the good word

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,198 @@
-function displayNextImage(){x=x===images.length-1?0:x+1,document.getElementById("schoolimg").src="img/"+images[x]+".jpg"}function moveMoon(){var a=$("#moon").css("backgroundPosition").split(" "),b=a[0];b=b.substring(0,b.length-1)-1,b<0&&(b=100),yPos=0,document.getElementById("moon").style.backgroundPosition=b+"% "+yPos+"%"}function startTimer(){setInterval(displayNextImage,1500)}function isElementInViewport(a){var b=$(a),c=navigator.userAgent.toLowerCase().indexOf("webkit")!=-1?"body":"html",d=$(c).scrollTop(),e=d+$(window).height(),f=Math.round(b.offset().top),g=f+b.height();return f<e&&g>d}function checkAnimation(a){if("skillspage"==a){var b=$(".skill-level");if(b.hasClass("start"))return;b.addClass("start")}else if("homepage"==a){var b=$("#home .characteristics");if(b.hasClass("start"))return;b.addClass("start")}else if("careerpage"==a){var b=$("#work .careerphoto");if(b.hasClass("start"))return;b.addClass("start")}else if("aboutmepage"==a){var b=$(".fly");if(b.hasClass("start"))return;b.addClass("start")}else if("schoolpage"==a){var b=$("#encapsulate");if(b.hasClass("start"))return;b.addClass("start");var b=$("#smart");if(b.hasClass("start"))return;b.addClass("start");var b=$("#teamplayer");if(b.hasClass("start"))return;b.addClass("start");var b=$("#tup");if(b.hasClass("start"))return;b.addClass("start");var b=$("#downloadresume");if(b.hasClass("start"))return;b.addClass("start")}else if("learningpage"==a){var b=$("#learningispassion");if(b.hasClass("start"))return;b.addClass("start");var b=$("#programming");if(b.hasClass("start"))return;b.addClass("start")}}function changeNav(a){var b=$(".navbar");1==a?b.css("background-color","rgba(0,0,0,0.8)"):2==a&&b.css("background-color","rgba(0,0,0,0)")}$(document).ready(function(){$("#fullpage").fullpage({anchors:["homepage","aboutmepage","schoolpage","learningpage","moonpage\t","skillspage","careerpage","freelancingpage","contactmepage"],menu:"#menu",onLeave:function(a,b,c){$(this);checkAnimation($(".fp-section").eq(b-1).attr("data-anchor"))},afterLoad:function(a,b){checkAnimation(a)},scrollingSpeed:500,verticalCentered:!1,navigation:!1,navigationPosition:"right",navigationTooltips:["Home","About Me","About Me","About Me","About Me","Skills","Career Background","Freelancing","Contact Me"],continuousHorizontal:!0}),$("a.photoslide").click(function(){$("a.photoslide").removeClass("selected"),$(this).hasClass("selected")||$(this).addClass("selected"),$("#caption").html($(this).attr("data-caption")),$("a.first img").removeClass("firstnotselected")}),$("a.photoslide.last").click(function(){$("a.first img").addClass("firstnotselected")})});var images=[],x=-1;images[0]="mead1",images[1]="mead2",images[2]="mead3",images[3]="mead4",images[4]="mead5",images[5]="mead6";
+$(document).ready(function()
+{
+	// var anchors = ['homepage', 'aboutmepage', 'aboutmepage', 'aboutmepage', 'aboutmepage', 'skillspage','careerpage', 'freelancingpage', 'contactmepage'];
+	$('#fullpage').fullpage({
+					// sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+					anchors: ['homepage', 'aboutmepage', 'schoolpage', 'learningpage', 'moonpage	', 'skillspage','careerpage', 'freelancingpage', 'contactmepage'],
+					menu: '#menu',
+					onLeave: function(index, nextIndex, direction){
+						var leavingSection = $(this);
+			            //after leaving section 2
+
+			            // console.log( $('.fp-section').eq(nextIndex - 1 ).attr('data-anchor') );
+			            checkAnimation( $('.fp-section').eq(nextIndex - 1 ).attr('data-anchor') );
+			            // if(index == 5 && direction =='down'){
+			            //     checkAnimation();
+			            // }
+			            // if(index == 5 && direction =='up'){
+			            //     checkAnimation();
+			            // }
+			            
+
+			            // changeNav(nextIndex);
+					},
+					afterLoad: function(anchorLink, index)
+					{
+						 
+			            checkAnimation(anchorLink);
+			            
+					},
+					scrollingSpeed: 500,
+					verticalCentered:false,
+					navigation: false,
+					navigationPosition: 'right',
+					navigationTooltips: ['Home', 'About Me','About Me','About Me','About Me', 'Skills', 'Career Background', 'Freelancing', 'Contact Me'],
+					continuousHorizontal: true
+				});
+
+	$("a.photoslide").click(function()
+	{
+		$("a.photoslide").removeClass("selected");
+		if ($(this).hasClass("selected"))
+		{
+			// $(this).removeClass(" selected");
+		}
+		else
+		{
+			$(this).addClass("selected");
+		}
+		$("#caption").html($(this).attr('data-caption'));
+
+		$("a.first img").removeClass("firstnotselected");	
+	});
+	$('#frmMsg').submit(function () {
+		alert('test');
+	 sendContactForm();
+	 return false;
+	});
+	$("a.photoslide.last").click(function()
+	{
+		$("a.first img").addClass("firstnotselected");
+	});
+
+});
+
+function displayNextImage() {
+  x = (x === images.length - 1) ? 0 : x + 1;
+  document.getElementById("schoolimg").src = "img/" + images[x] + ".jpg";
+  // document.getElementById(images[0]).style.opacity="0";
+  // document.getElementById(images[1]).style.opacity="0";
+  // document.getElementById(images[2]).style.opacity="0";
+  // document.getElementById(images[3]).style.opacity="0";
+  // document.getElementById(images[4]).style.opacity="0";
+  // document.getElementById(images[5]).style.opacity="0";
+  // document.getElementById(images[x]).style.opacity="1"
+}
+
+function moveMoon()
+{
+	var backgroundPos = $("#moon").css('backgroundPosition').split(" ");
+	var xPos = backgroundPos[0];
+	xPos = xPos.substring(0, xPos.length-1) - 1;
+	if(xPos<0)
+	{
+		xPos=100;
+	}
+	yPos=0;
+	document.getElementById("moon").style.backgroundPosition = xPos + "% " + yPos +"%";
+}
+
+function startTimer() {
+  setInterval(displayNextImage, 1500);
+  // setInterval(moveMoon, 1000);
+}
+
+var images = [], x = -1;
+images[0] = "mead1";
+images[1] = "mead2";
+images[2] = "mead3";
+images[3] = "mead4";
+images[4] = "mead5";
+images[5] = "mead6";
+
+// Returns true if the specified element has been scrolled into the viewport.
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation(anchorLink) {
+
+	if (anchorLink == 'skillspage')
+	{
+		var $elem = $('.skill-level');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+	}
+	else if (anchorLink == 'homepage')
+	{
+
+		var $elem = $('#home .characteristics');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+	}
+	else if (anchorLink == 'careerpage')
+	{
+
+		var $elem = $('#work .careerphoto');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+	}
+	else if (anchorLink == 'aboutmepage')
+	{
+
+		var $elem = $('.fly');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+			
+	}
+	else if (anchorLink == 'schoolpage')
+	{
+		var $elem = $('#encapsulate');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+		
+		var $elem = $('#smart');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+		
+		var $elem = $('#teamplayer');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+		
+		var $elem = $('#tup');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+		
+		var $elem = $('#downloadresume');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+	}
+	else if (anchorLink == 'learningpage')
+	{
+		var $elem = $('#learningispassion');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+		
+		var $elem = $('#programming');
+		if ($elem.hasClass('start')) return;
+		$elem.addClass('start');
+	}
+		
+
+}
+function changeNav(nextIndex)
+{
+	
+	var $nav = $('.navbar');
+	if (nextIndex==1)
+		$nav.css('background-color', 'rgba(0,0,0,0.8)');
+	else if (nextIndex==2)
+		$nav.css('background-color', 'rgba(0,0,0,0)');
+}
+
+// Capture scroll events
+// $(window).scroll(function(){
+//     checkAnimation();
+// });
